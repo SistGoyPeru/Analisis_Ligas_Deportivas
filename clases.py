b645bb11a76f8ff344fddd0caea31453590460be
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-05-19 12:33:35 +0000

    poniendo ligas

--- clases.py
+++ clases.py
@@ -21,21 +21,31 @@ class Ligas:
         self.archivo=archivo
         self.df_fixture=pd.read_excel(Path(__file__).resolve().parent / self.archivo)
 
-    def equipos_local(self):
-        return self.df_fixture["Local"].unique()
+    def ligas_paises(self):
+        return self.df_fixture["Liga"].unique()
     
-    def equipos_visita(self):
-        return self.df_fixture["Visita"].unique()
+    def equipos_local(self,liga):
+        df_liga=self.df_fixture[self.df_fixture["Liga"]==liga]
+        return df_liga["Local"].unique()
+    
+    def equipos_visita(self,liga):
+        df_liga=self.df_fixture[self.df_fixture["Liga"]==liga]
+        return df_liga["Visita"].unique()
         
-    def PgaLG(self):
-        return self.df_fixture["GF"].mean() 
-    def PgcLG(self):
-        return self.df_fixture["GC"].mean()
+        
+    def PgaLG(self,liga):
+        df_liga=self.df_fixture[self.df_fixture["Liga"]==liga]
+        return df_liga["GF"].mean() 
+    def PgcLG(self,liga):
+        df_liga=self.df_fixture[self.df_fixture["Liga"]==liga]
+        return df_liga["GC"].mean()
     
-    def PgaVG(self):
-        return self.df_fixture["GC"].mean() 
-    def PgcVG(self):
-        return self.df_fixture["GF"].mean()
+    def PgaVG(self,liga):
+        df_liga=self.df_fixture[self.df_fixture["Liga"]==liga]
+        return df_liga["GC"].mean() 
+    def PgcVG(self,liga):
+        df_liga=self.df_fixture[self.df_fixture["Liga"]==liga]
+        return df_liga["GF"].mean()
     
     def PgaL(self,local):
         return self.df_fixture[self.df_fixture['Local']==local].groupby('Local')['GF'].mean().iloc[0]
@@ -47,101 +57,101 @@ class Ligas:
     def PgcV(self,visita):
         return self.df_fixture[self.df_fixture['Visita']==visita].groupby('Visita')['GF'].mean().iloc[0]
     
-    def FoL(self,local):
-        return self.PgaL(local)/self.PgaLG()
-    def FdL(self,local):
-        return self.PgcL(local)/self.PgcLG()
+    def FoL(self,local,liga):
+        return self.PgaL(local)/self.PgaLG(liga)
+    def FdL(self,local,liga):
+        return self.PgcL(local)/self.PgcLG(liga)
     
-    def FoV(self,visita):
-        return self.PgaV(visita)/self.PgaVG()
-    def FdV(self,visita):
-        return self.PgcV(visita)/self.PgcVG()
+    def FoV(self,visita,liga):
+        return self.PgaV(visita)/self.PgaVG(liga)
+    def FdV(self,visita,liga):
+        return self.PgcV(visita)/self.PgcVG(liga)
     
-    def FpL(self,local,visita):
-        return self.FoL(local)*self.FdV(visita)
-    def FpV(self,local,visita):
-        return self.FoV(visita)*self.FdL(local)
+    def FpL(self,local,visita,liga):
+        return self.FoL(local,liga)*self.FdV(visita,liga)
+    def FpV(self,local,visita,liga):
+        return self.FoV(visita,liga)*self.FdL(local,liga)
     
     def GeL(self,gol,fpl):
         return poisson.pmf(gol,fpl)
     def GeV(self,gol,fpv):
         return poisson.pmf(gol,fpv)
     
-    def Pos_empate(self,local,visita):
+    def Pos_empate(self,local,visita,liga):
         suma=0
         for x in range(11):
-            suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(x,self.FpV(local,visita))
+            suma=suma+self.GeL(x,self.FpL(local, visita,liga))*self.GeV(x,self.FpV(local,visita,liga))
         return suma  
     
-    def PosG_local(self,local,visita):
+    def PosG_local(self,local,visita,liga):
         suma=0
         for x in range(1,11):          
             for i in range(10):         
                 if x>i:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local,visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma 
     
-    def PosG_visita(self,local,visita):
+    def PosG_visita(self,local,visita,liga):
         suma=0
         for x in range(10):          
             for i in range(1,11):         
                 if x<i:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local,visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma  
     
-    def May05(self,local,visita):
+    def May05(self,local,visita,liga):
         suma=0
         for x in range(11):          
             for i in range(11):         
                 if x+i>=1:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local,visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma
     
-    def May15(self,local,visita):
+    def May15(self,local,visita,liga):
         suma=0
         for x in range(11):          
             for i in range(11):         
                 if x+i>=2:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local, visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma
     
-    def May25(self,local,visita):
+    def May25(self,local,visita,liga):
         suma=0
         for x in range(11):          
             for i in range(11):         
                 if x+i>=3:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local, visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma 
     
-    def May35(self,local,visita):
+    def May35(self,local,visita,liga):
         suma=0
         for x in range(11):          
             for i in range(11):         
                 if x+i>=4:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local, visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma 
     
-    def May45(self,local,visita):
+    def May45(self,local,visita,liga):
         suma=0
         for x in range(11):          
             for i in range(11):         
                 if x+i>=5:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local, visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma 
     
-    def May55(self,local,visita):
+    def May55(self,local,visita,liga):
         suma=0
         for x in range(11):          
             for i in range(11):         
                 if x+i>=6:
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local, visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma
     
-    def AmbosMarcan(self,local,visita):
+    def AmbosMarcan(self,local,visita,liga):
         suma=0
         for x in range(1,11):          
             for i in range(1,11):         
-                    suma=suma+self.GeL(x,self.FpL(local, visita))*self.GeV(i,self.FpV(local,visita))
+                    suma=suma+self.GeL(x,self.FpL(local, visita,liga))*self.GeV(i,self.FpV(local,visita,liga))
         return suma
    
     
--- main.py
+++ main.py
@@ -17,11 +17,15 @@ df=Ligas("Ligas_de_futbol.xlsm")
 
 def main():
     st.sidebar.header("Ligas de Futbol")
-    ligas=st.sidebar.selectbox("Liga a Pronosticar",df.equipos_local())
+    ligas=st.sidebar.selectbox("Liga a Pronosticar",df.ligas_paises())
     st.sidebar.header("Encuentros")
-    local=st.sidebar.selectbox("Equipo Local",df.equipos_local())
-    visita=st.sidebar.selectbox("Equipo Visita",df.equipos_visita())
+    local=st.sidebar.selectbox("Equipo Local",df.equipos_local(ligas))
+    visita=st.sidebar.selectbox("Equipo Visita",df.equipos_visita(ligas))
     st.markdown("## "+local+" - "+visita)
+   
+    st.write(df.FoL(local,ligas))
+    st.write(df.FdV(visita,ligas))
+    st.write(df.FpL(local,visita,ligas))
     
     
     
