23bf55455afee2154a11e53ddc485c9ab4fbb089
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-05-21 18:09:33 +0000

    actua

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
     with col1:
         resultados = ['Victoria '+local, 'Empate', 'Victoria '+visita]
         probabilidades = [df.PosG_local(local,visita,ligas)*100, df.Pos_empate(local,visita,ligas)*100, df.PosG_visita(local,visita,ligas)*100]
-        colores = ['blue', 'gray', 'green']
+        colores = ['green', 'Yellow', 'red']
         plt.figure(figsize=(12, 8))
         bars=plt.bar(resultados, probabilidades, color=colores, alpha=0.7)
         plt.ylabel('Probabilidad (%)',fontsize=20)
@@ -49,16 +49,30 @@ def main():
         plt.xticks(fontsize=16)
         plt.tight_layout()
         st.pyplot(plt)
-    
-    with col2:
 
         resultados = ['>0.5', '>1.5', '>2.5','>3.5','>4.5','>5.5']
         probabilidades = [df.May05(local,visita,ligas)*100,df.May15(local,visita,ligas)*100,df.May25(local,visita,ligas)*100,df.May35(local,visita,ligas)*100,df.May45(local,visita,ligas)*100,df.May55(local,visita,ligas)*100, ]
-        colores = ['blue', 'gray', 'green']
+        colores = ['#28b463', '#2ecc71', '#58d68d','#82e0aa', '#abebc6', '#d5f5e3']
         plt.figure(figsize=(12, 8))
         bars=plt.bar(resultados, probabilidades, color=colores, alpha=0.7)
         plt.ylabel('Probabilidad (%)',fontsize=20)
         plt.title('Probabilidad de Goles marcados ',fontsize=20)
+        plt.grid(axis='y', linestyle='--',)
+        plt.bar_label(bars, padding=3,fmt='%.2f%%',fontsize=20)
+        plt.yticks(range(0, 101, 10), [f'{i}%' for i in range(0, 101, 10)],fontsize=20)
+        plt.xticks(fontsize=16)
+        plt.tight_layout()
+        st.pyplot(plt)
+    
+    with col2:
+
+        resultados = ['1X', '12', 'X2']
+        probabilidades = [df.PosG_local(local,visita,ligas)*100+df.Pos_empate(local,visita,ligas)*100,df.PosG_local(local,visita,ligas)*100+df.PosG_visita(local,visita,ligas)*100,df.Pos_empate(local,visita,ligas)*100+df.PosG_visita(local,visita,ligas)*100]
+        colores = ['green', 'Yellow', 'red']
+        plt.figure(figsize=(12, 8))
+        bars=plt.bar(resultados, probabilidades, color=colores, alpha=0.7)
+        plt.ylabel('Probabilidad (%)',fontsize=20)
+        plt.title('Probabilidad Doble Oportunidad',fontsize=20)
         plt.grid(axis='y', linestyle='--',)
         plt.bar_label(bars, padding=3,fmt='%.2f%%',fontsize=20)
         plt.yticks(range(0, 101, 10), [f'{i}%' for i in range(0, 101, 10)],fontsize=20)
